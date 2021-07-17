277a0e64c8f347682a10114405436743c2e2f0d9
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-17 11:22:37 +0100

    project setup

---
+++ test/test.js
@@ -0,0 +1,66 @@
+class Matchers {
+  constructor(example) {
+    this.example = example;
+  }
+
+  is(expectation) {
+    if (this.example === expectation) {
+      console.log("All good, dude!");
+    } else {
+      throw new Error(
+        `You better chat with your rubber-duck! We were expecting: ${expectation}, but got ${this.example} instead.`
+      );
+    }
+  }
+
+  isNot(expectation) {
+    if (this.example !== expectation) {
+      console.log(`${example} is definitively not ${expectation}`);
+    }
+  }
+
+  isForReal() {
+    if (this.example) {
+      console.log(`This is true, scout's honor`);
+    } else {
+      throw new Error(
+        `Why you lying? I thought we were friends. ${this.example} is nothing more than fugazzi!`
+      );
+    }
+  }
+  
+  isFullOfLies() {
+    if (!this.example) {
+      console.log(
+        `This is nothing more than a well elaborated misconstruction of the truth`
+      );
+    } else {
+      throw new Error(
+        `Who knew honesty still shines. ${this.example} is being truthful right here`
+      );
+    }
+  }
+}
+
+function checkIf(example) {
+  return new Matchers(example);
+}
+
+function it(testName, fn) {
+  console.log(`test: ${testName}`);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err);
+    throw new Error(`this test literally failed`);
+  }
+}
+
+function verify(suiteName, fn) {
+  try {
+    console.log(`suite: ${suiteName}`);
+    fn();
+  } catch (err) {
+    console.log(err.mesage);
+  }
+}
